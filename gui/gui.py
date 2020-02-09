79d7d08df9f249600d9b1764398b5ea5fd3b2c28
6birkenh <6birkenh@informatik.uni-hamburg.de>
2020-02-09 22:08:56 +0100

    big refactor of gui.py

--- gui/gui.py
+++ gui/gui.py
@@ -1,71 +1,99 @@
 import tkinter as tk
 import numpy as np
 #from ..grid.grid import gamegrid
-
+#TODO: dictionary for colours => cell_status
 
 class gui(object):
-    def __init__(self, state):
-        self.state = state
-        self.cells = np.zeros_like(self.state, dtype=object)
-        self.update = False
-        self.running = True
+    """ Main windows for game of life.
+
+    Handles board state and updates of cells.
+    """
+    def __init__(self, board_state):
+        self.board_state = board_state
+        self.board_size = (np.shape(self.board_state)[0],
+                            np.shape(self.board_state)[1])
+        self.auto_update_state = False
+        self.app_running = True
+
+        self.init_main_window()
+
+    def init_main_window(self):
+        # root window
         self.root = tk.Tk()
         self.root.title("Conway's Game of Life")
+
+        # frame for all cell buttons
         self.master = tk.Frame(self.root)
         self.master.pack()
-        self.update_checkbox = tk.Button(self.root, text="Let Life flow", command=(lambda:self.update_runner()))
-        self.update_checkbox.pack()
-        self.create_cells()
 
-    def create_cells(self):
-        for x in range(np.shape(self.state)[0]):
-            for y in range(np.shape(self.state)[1]):
+        # Buttons
+        self.auto_update_button = tk.Button(self.root, text="Let Life flow", 
+                                        command=(lambda:self.auto_update_toggle()))
+        self.auto_update_button.pack()
+        self.initialize_cell_buttons()
+
+    def initialize_cell_buttons(self):
+        """ Creates tk Buttons and stores them in self.buttons.
+
+        Each button is assigned a cell with it's corresponding indices.
+        """
+        self.cell_buttons = np.zeros_like(self.board_state, dtype=object)
+
+        for x in range(self.board_size[0]):
+            for y in range(self.board_size[1]):
+                # each frame holds its button
                 cellframe = tk.Frame(self.master, height=20, width=20)
-                cellframe.grid_propagate(False) #disables resizing of frame
-                cellframe.columnconfigure(0, weight=1) #enables button to fill frame
-                cellframe.rowconfigure(0,weight=1) #any positive number would do the trick  
-                cellframe.grid(row=x, column=y)
+                cellframe.grid_propagate(False) # disables resizing of frame
+                cellframe.columnconfigure(0, weight=1) # enables button to fill frame
+                cellframe.rowconfigure(0,weight=1) # any positive number would do the trick  
+                cellframe.grid(row=x, column=y) # place frame according to boardstate
 
-                cellbutton = tk.Button(cellframe, bg='gray25', command=(lambda x=x, y=y: self.button_press(x,y)))
-                cellbutton.grid(sticky='wens') #expanding buttons
-                self.cells[x][y] = cellbutton
-                
+                cellbutton = tk.Button(cellframe, bg='gray25', 
+                                    command=(lambda x=x, y=y: self.cell_button_press(x,y)))
+                cellbutton.grid(sticky='wens') # expanding buttons
 
-                #self.cells[x][y] = tk.Button(self.master, bg='gray25', fg='red2', padx=1, pady=1, height=5, width=5)
-                #self.cells[x][y].grid(row=x, column=y)
-    
-    def update_cells(self):
-        for x in range(np.shape(self.state)[0]):
-            for y in range(np.shape(self.state)[1]):
-                cell_status = self.state[x][y]
+                # save buttons for later configurations
+                self.cell_buttons[x][y] = cellbutton
+
+    def update_cell_buttons(self):
+        """ Updates the colours of each Button.
+
+        Cell status is retrieved via self.state.
+        """
+        for x in range(self.board_size[0]):
+            for y in range(self.board_size[1]):
+                cell_status = self.board_state[x][y]
                 if cell_status == 1:
-                    self.cells[x][y].config(bg='red2')
+                    self.cell_buttons[x][y].config(bg='red2')
                 if cell_status == 0:
-                    self.cells[x][y].config(bg='gray25')
+                    self.cell_buttons[x][y].config(bg='gray25')
 
-
-    def button_press(self, x, y):
-        button = self.cells[x][y]
-        cell_status = self.state[x][y]
+    def cell_button_press(self, x, y):
+        """ On button press updates board_state.
+        """
+        button = self.cell_buttons[x][y]
+        cell_status = self.board_state[x][y]
         if cell_status == 1:
             button.config(bg='gray25')
-            self.state[x][y] = 0
+            self.board_state[x][y] = 0
         if cell_status == 0:
             button.config(bg="red2")
-            self.state[x][y] = 1
+            self.board_state[x][y] = 1
     
-    def set_state(self, state):
-        self.state = state
-        self.update_cells()
+    def set_state(self, board_state):
+        """ Set board_state and update cell buttons.
+        """
+        #TODO: try catch, size of board_state has to remain the same
+        self.board_state = board_state
+        self.update_cell_buttons()
     
-    def update_runner(self):
-        self.update = not self.update
-
-        
-
+    def auto_update_toggle(self):
+        """ Decide to auto update cells, or not.
+        """
+        self.auto_update_state = not self.auto_update_state
 
 if __name__ == "__main__":
-    game_gui = gui(state=[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    game_gui = gui(board_state=[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
@@ -75,11 +103,13 @@ if __name__ == "__main__":
                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 1, 1, 1, 0, 0, 0, 0, 0]])
-    game_gui.update_cells()
+    game_gui.update_cell_buttons()
 
-    while(game_gui.running):
+    while(game_gui.app_running):
     
         game_gui.root.update()
         game_gui.root.update_idletasks()
-        while(game_gui.update):
-            game_gui.update_cells()
+        while(game_gui.auto_update_state):
+            game_gui.root.update()
+            game_gui.root.update_idletasks()
+            game_gui.update_cell_buttons()
--- main.py
+++ main.py
@@ -17,22 +17,22 @@ if __name__ == "__main__":
     game_gui = gui(game_grid.grid)
 
 
-    while(game_gui.running):
+    while(game_gui.app_running):
         game_gui.root.update()
         game_gui.root.update_idletasks()
-        game_gui.update_cells() #update gui
-        print(game_gui.update)
+        game_gui.update_cell_buttons() #update gui
+        print(game_gui.auto_update_state)
         
-        while(game_gui.update):
+        while(game_gui.auto_update_state):
             game_gui.root.update()
             game_gui.root.update_idletasks()
 
-            game_grid.set_state(game_gui.state) #get from gui
+            game_grid.set_state(game_gui.board_state) #get from gui
             game_grid.next_state_of_grid() #calc next state
-            game_gui.state = game_grid.grid #get from grid
-            game_gui.update_cells() #update gui
+            game_gui.board_state = game_grid.grid #get from grid
+            game_gui.update_cell_buttons() #update gui
             time.sleep(0.1)
-            print(game_gui.update)
+            print(game_gui.auto_update_state)
 
 
 
