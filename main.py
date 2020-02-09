be3e13a1edf500be3d7e8f2f4c9f96d4995becc4
6birkenh <6birkenh@informatik.uni-hamburg.de>
2020-02-09 22:42:21 +0100

    big refactor of grid => board_logic.py

--- main.py
+++ main.py
@@ -1,38 +1,38 @@
-from grid.grid import grid
+from game_logic.board_logic import board_logic
 from gui.gui import gui
 import time
 
 
 if __name__ == "__main__":
-    game_grid = grid(state=[[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 1, 1, 1, 0, 0, 0, 0, 0]])
-    game_gui = gui(game_grid.grid)
+    game_logic = board_logic(board_state=  [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                            [0, 0, 1, 1, 1, 0, 0, 0, 0, 0]])
+    game_gui = gui(game_logic.board_state)
 
-
+    #TODO: use events and eventlisteners!
     while(game_gui.app_running):
         game_gui.root.update()
         game_gui.root.update_idletasks()
         game_gui.update_cell_buttons() #update gui
-        print(game_gui.auto_update_state)
+
         
         while(game_gui.auto_update_state):
             game_gui.root.update()
             game_gui.root.update_idletasks()
 
-            game_grid.set_state(game_gui.board_state) #get from gui
-            game_grid.next_state_of_grid() #calc next state
-            game_gui.board_state = game_grid.grid #get from grid
-            game_gui.update_cell_buttons() #update gui
-            time.sleep(0.1)
-            print(game_gui.auto_update_state)
+            game_logic.set_state(game_gui.board_state) # GUI => LOGIC
+            game_logic.calculate_next_board_state() # LOGIC calculates
+            game_gui.board_state = game_logic.board_state # LOGIC => GUI
+            game_gui.update_cell_buttons() # GUI update
+            #time.sleep(0.1)
+
 
 
 
